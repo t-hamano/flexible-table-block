456d7e01c09d4d1824a152721b25e1a9a9dcf7e5
Hiroshi Urabe <mail@torounit.com>
2021-10-06 12:38:37 +0900

    add unit test and test workflow

---
+++ src/utils/test/style-picker.test.js
@@ -0,0 +1,12 @@
+import { pickPadding } from '../style-picker';
+
+describe( 'style-picker', () => {
+	it( 'should separate shorthand prop', () => {
+		expect( pickPadding( { padding: '10px' } ) ).toBe( {
+			bottom: '10px',
+			left: '10px',
+			right: '10px',
+			top: '10px',
+		} );
+	} );
+} );
