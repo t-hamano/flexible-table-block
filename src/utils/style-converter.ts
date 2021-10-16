c258d85aa58d8c5418ef0f68591bb4c4570de291
Hiroshi Urabe <mail@torounit.com>
2021-10-16 15:40:34 +0900

    use ts for style-converter and add tests.

--- src/utils/style-converter.ts
+++ src/utils/style-converter.ts
@@ -1,11 +1,13 @@
+import type { Properties } from 'csstype';
+
 /**
  * Convert inline CSS styles to object.
  *
- * @param {string} inlineStyles Inline CSS styles.
- * @return {Object} CSS styles object.
+ * @param  inlineStyles Inline CSS styles.
+ * @return CSS styles object.
  */
-export function convertToObject( inlineStyles ) {
-	if ( ! inlineStyles || typeof inlineStyles !== 'string' ) {
+export function convertToObject( inlineStyles: string ): Properties {
+	if ( ! inlineStyles ) {
 		return {};
 	}
 
@@ -31,23 +33,23 @@ export function convertToObject( inlineStyles ) {
 /**
  * Convert CSS styles object to Inline CSS styles.
  *
- * @param {Object} stylesObj CSS styles object.
- * @return {string} Inline CSS styles
+ * @param  stylesObj CSS styles object.
+ * @return Inline CSS styles
  */
-export function convertToInline( stylesObj ) {
-	if ( ! stylesObj || typeof stylesObj !== 'object' ) {
-		return '';
-	}
-
-	const lines = Object.keys( stylesObj ).reduce( function ( result, key ) {
+export function convertToInline( stylesObj: Properties ): string {
+	const lines: string[] = Object.keys( stylesObj ).reduce< string[] >( function (
+		result: string[],
+		key: string
+	) {
 		const property = key.replace( /([a-z])([A-Z])/g, '$1-$2' ).toLowerCase();
-		const value = stylesObj[ key ];
+		const value = stylesObj[ key as keyof Properties ];
 
 		if ( value !== undefined && ( typeof value === 'string' || value === 0 ) ) {
 			result.push( `${ property }:${ value };` );
 		}
 		return result;
-	}, [] );
+	},
+	[] as string[] );
 
 	return lines.join( '' );
 }
