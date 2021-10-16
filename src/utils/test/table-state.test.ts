3fc34b72b14508e6f37f5f43b3dda79fc3de6021
Hiroshi Urabe <mail@torounit.com>
2021-10-16 10:53:58 +0900

    refactor test

--- src/utils/test/table-state.test.ts
+++ src/utils/test/table-state.test.ts
@@ -1,23 +1,28 @@
 import { createTable } from '../table-state';
-import type { Cell } from '../VirtualTable';
+import type { Cell, Row } from '../VirtualTable';
+import { times } from 'lodash';
+
+const getCell = ( tag: 'th' | 'td', content = '' ): Cell => {
+	return {
+		content,
+		tag,
+	};
+};
+
+const getRow = ( cells: number, tag: 'th' | 'td', content = '' ): Row => {
+	return {
+		cells: times( cells, () => getCell( tag, content ) ),
+	};
+};
 
 describe( 'table-state', () => {
 	describe( 'createTable', () => {
-		const getCell = ( tag: 'th' | 'td' ): Cell => {
-			return {
-				content: '',
-				tag,
-			};
-		};
-		const row = {
-			cells: [ getCell( 'td' ), getCell( 'td' ) ],
-		};
 		it( 'should create the right virtual table', () => {
 			expect(
 				createTable( { rowCount: 2, colCount: 2, headerSection: false, footerSection: false } )
 			).toStrictEqual( {
 				head: [],
-				body: [ row, row ],
+				body: [ getRow( 2, 'td' ), getRow( 2, 'td' ) ],
 				foot: [],
 			} );
 		} );
@@ -26,13 +31,9 @@ describe( 'table-state', () => {
 			expect(
 				createTable( { rowCount: 2, colCount: 2, headerSection: true, footerSection: true } )
 			).toStrictEqual( {
-				head: [
-					{
-						cells: [ getCell( 'th' ), getCell( 'th' ) ],
-					},
-				],
-				body: [ row, row ],
-				foot: [ row ],
+				head: [ getRow( 2, 'th' ) ],
+				body: [ getRow( 2, 'td' ), getRow( 2, 'td' ) ],
+				foot: [ getRow( 2, 'td' ) ],
 			} );
 		} );
 	} );
