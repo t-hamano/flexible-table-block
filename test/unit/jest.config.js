fe3165da144ac9382630056a7142b54e40f90ac0
Hiroshi Urabe <mail@torounit.com>
2021-10-17 00:51:15 +0900

    set ignore dire

--- test/unit/jest.config.js
+++ test/unit/jest.config.js
@@ -3,4 +3,5 @@ const config = require( '@wordpress/scripts/config/jest-unit.config.js' );
 module.exports = {
 	...config,
 	rootDir: '../../',
+	testPathIgnorePatterns: [ '<rootDir>/test/e2e' ],
 };
