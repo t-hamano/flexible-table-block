344d2246813020be6f3506aa9aee8fc279a8fa55
Tetsuaki Hamano / 浜野 哲明 <tetsuaki.hamano@gmail.com>
2021-10-21 03:55:29 +0000

    v2.0.1

--- flexible-table-block.php
+++ flexible-table-block.php
@@ -4,7 +4,7 @@
  * Description: Easily create flexible configuration tables.
  * Requires at least: 5.8
  * Requires PHP: 7.3
- * Version: 2.0.0
+ * Version: 2.0.1
  * Author: Tetsuaki Hamano
  * Author URI: https://github.com/t-hamano
  * License: GPL2 or later
