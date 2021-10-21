663e84b274f3bf0aa68ad9dee77562c6d5551db1
Hiroshi Urabe <mail@torounit.com>
2021-10-21 20:39:07 +0900

    convert transform to ts

--- src/transforms.tsx
+++ src/transforms.tsx
@@ -2,6 +2,7 @@
  * WordPress dependencies
  */
 import { createBlock } from '@wordpress/blocks';
+import type { TransformBlock } from '@wordpress/blocks';
 
 /**
  * Internal dependencies
@@ -11,14 +12,21 @@ import {
 	toTableAttributes,
 	toVirtualRows,
 	toVirtualTable,
+	VCell,
 } from './utils/table-state';
+import type { BlockAttributes, CoreTableBlockAttributes } from './BlockAttributes';
 
-const transforms = {
+interface Transforms {
+	readonly from: ReadonlyArray< TransformBlock< CoreTableBlockAttributes > >;
+	readonly to: ReadonlyArray< TransformBlock< BlockAttributes > >;
+}
+
+const transforms: Transforms = {
 	from: [
 		{
 			type: 'block',
 			blocks: [ 'core/table' ],
-			transform: ( attributes ) => {
+			transform: ( attributes: CoreTableBlockAttributes ) => {
 				const { hasFixedLayout, head, body, foot, caption } = attributes;
 				return createBlock( 'flexible-table-block/table', {
 					hasFixedLayout,
@@ -34,7 +42,7 @@ const transforms = {
 		{
 			type: 'block',
 			blocks: [ 'core/table' ],
-			transform: ( attributes ) => {
+			transform: ( attributes: BlockAttributes ) => {
 				// Create virtual object array with the cells placed in positions based on how they actually look.
 				let vTable = toVirtualTable( attributes );
 
@@ -42,7 +50,7 @@ const transforms = {
 				const vRows = toVirtualRows( vTable );
 
 				const rowColSpanCells = vRows
-					.reduce( ( cells, row ) => cells.concat( row.cells ), [] )
+					.reduce( ( cells: VCell[], row ) => cells.concat( row.cells ), [] )
 					.filter( ( { rowSpan, colSpan } ) => rowSpan > 1 || colSpan > 1 );
 				// Split the found rowspan & colspan cells.
 				if ( rowColSpanCells.length ) {
