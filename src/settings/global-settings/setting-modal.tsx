7d582cfb3d6e5e4115283288f3f94abefe29966f
Tetsuaki Hamano <tetsuaki.hamano@gmail.com>
2023-07-27 13:17:38 +0900

    Fix ewe test failure

--- src/settings/global-settings/setting-modal.tsx
+++ src/settings/global-settings/setting-modal.tsx
@@ -83,7 +83,7 @@ export default function SettingModal( { options, isAdministrator, setIsSettingMo
 	// Update the inline CSS.
 	function updateInlineCss( css: string ) {
 		// Update the inline CSS of the global document.
-		const styleSheet = document.getElementById( 'flexible-table-block-editor-inline-css' );
+		const styleSheet = document.getElementById( 'flexible-table-block-table-editor-style-css' );
 
 		if ( styleSheet ) {
 			styleSheet.textContent = css;
@@ -98,7 +98,7 @@ export default function SettingModal( { options, isAdministrator, setIsSettingMo
 			if ( ! iframeWindow ) continue;
 
 			const iframeStyleSheet = iframeWindow.document.getElementById(
-				'flexible-table-block-editor-inline-css'
+				'flexible-table-block-table-editor-style-css'
 			);
 
 			if ( iframeStyleSheet ) {
