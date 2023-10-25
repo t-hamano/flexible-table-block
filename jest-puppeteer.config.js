ecfcd46004fc920331eff125b1a1cdaff6132f30
Tetsuaki Hamano <tetsuaki.hamano@gmail.com>
2023-10-25 17:13:19 +0900

    Remove unused file

--- jest-puppeteer.config.js
+++
@@ -1,9 +0,0 @@
-const config = require( '@wordpress/scripts/config/puppeteer.config' );
-
-module.exports = {
-	...config,
-	launch: {
-		...config.launch,
-		slowMo: parseInt( process.env.PUPPETEER_SLOWMO, 10 ) || 50,
-	},
-};
