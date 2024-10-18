92bce27ebd1d3c4782e3daf64382484c7e245509
Aki Hamano <tetsuaki.hamano@gmail.com>
2024-10-18 10:58:05 +0900

    Tweak indicator icons

--- src/controls/styles.tsx
+++ src/controls/styles.tsx
@@ -26,66 +26,66 @@ const Corner = styled.span< { isFocused: boolean } >`
 	display: block;
 	pointer-events: none;
 	position: absolute;
-	width: 9px;
-	height: 9px;
+	width: 8px;
+	height: 8px;
 	${ ( { isFocused } ) => ! isFocused && css( { opacity: 0.3 } ) }
 `;
 
 export const TopStroke = styled( Side )`
-	top: 2px;
-	right: 5px;
-	left: 5px;
+	top: 3px;
+	right: 6px;
+	left: 6px;
 	height: 2px;
 `;
 
 export const RightStroke = styled( Side )`
-	top: 5px;
-	right: 2px;
-	bottom: 5px;
+	top: 6px;
+	right: 3px;
+	bottom: 6px;
 	width: 2px;
 	background-color: currentColor;
 `;
 
 export const BottomStroke = styled( Side )`
-	right: 5px;
-	bottom: 2px;
-	left: 5px;
+	right: 6px;
+	bottom: 3px;
+	left: 6px;
 	height: 2px;
 	background-color: currentColor;
 `;
 
 export const LeftStroke = styled( Side )`
-	top: 5px;
-	bottom: 5px;
-	left: 2px;
+	top: 6px;
+	bottom: 6px;
+	left: 3px;
 	width: 2px;
 	background-color: currentColor;
 `;
 
 export const TopLeftStroke = styled( Corner )`
-	top: 2px;
-	left: 2px;
+	top: 3px;
+	left: 3px;
 	border-top: 2px solid;
 	border-left: 2px solid;
 `;
 
 export const TopRightStroke = styled( Corner )`
-	top: 2px;
-	right: 2px;
+	top: 3px;
+	right: 3px;
 	border-top: 2px solid;
 	border-right: 2px solid;
 `;
 
 export const BottomRightStroke = styled( Corner )`
-	right: 2px;
-	bottom: 2px;
+	right: 3px;
+	bottom: 3px;
 	border-right: 2px solid;
 	border-bottom: 2px solid;
 `;
 
 export const BottomLeftStroke = styled( Corner )`
-	bottom: 2px;
-	left: 2px;
+	bottom: 3px;
+	left: 3px;
 	border-bottom: 2px solid;
 	border-left: 2px solid;
 `;
