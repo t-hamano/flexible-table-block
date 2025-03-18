620059457f7be891be2a7c0449d75064fd66267a
Aki Hamano <tetsuaki.hamano@gmail.com>
2025-03-18 14:10:04 +0900

    Drop support for WP 6.5

--- flexible-table-block.php
+++ flexible-table-block.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Flexible Table Block
  * Description: Easily create flexible configuration tables.
- * Requires at least: 6.5
+ * Requires at least: 6.6
  * Requires PHP: 7.4
  * Version: 3.4.0
  * Author: Aki Hamano
